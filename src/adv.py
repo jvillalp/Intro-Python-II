53ab226e085d0fb965864bdfa6ef3a97165a8330
Juana Villalpando <juana.v14@gmail.com>
2020-06-04 13:23:29 -0700

    display items in rooms

--- src/adv.py
+++ src/adv.py
@@ -1,24 +1,25 @@
 from room import Room
 from player import Player
+from item import Item
 # Declare all the rooms
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+                     "North of you, the cave mount beckons", []),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east.""", []),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""", []),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+to north. The smell of gold permeates the air.""", []),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south.""", []),
 }
 
 
@@ -36,16 +37,27 @@ room['treasure'].s_to = room['narrow']
 #
 # Main
 #
-
+room['outside'].items.append(Item("Sword", "Used to kill the spiders."))
+room['outside'].items.append(Item("Stick", "used to hit rock."))
+room['foyer'].items.append(Item("Rock", "To open doors."))
 # Make a new player object that is currently in the 'outside' room.
 print("What is your name? ")
 name = input()
 
-player = Player(name, room['outside'])
+player = Player(name, room['outside'], [])
+
 # Write a loop that:
-while True:
+user_is_playng = True
+
+while user_is_playng:
     print("Current Room: " + player.current_room.name)
     print("\nDescription: " + player.current_room.description)
+    print("\nRoomItems: ")
+    for item in player.current_room.items:
+        print(item)
+    print("\nPlayerItems: ")
+    for item in player.items:
+        print(item)
     print("\nWhat direction would you like to go? Use N,S,E,W for directions, and q to quit.")
     direction = input()
     currentRoom = player.current_room.name
---
+++ src/item.py
@@ -0,0 +1,6 @@
+class Item:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+    def __str__(self):
+        return "Name: " + self.name + "\n" "Description: " + self.description
